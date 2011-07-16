d46d2d0b86bc1187a5c3ff03de001410762f26c4
ohtorii <ohtorii>
2011-07-16 11:51:48 +0900

    add sample.

--- clang-interpreter/data/sample_1.cpp
+++ clang-interpreter/data/sample_1.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char*argv[]){
 	FILE* fp=fopen("out.txt","wt");
-	fprintf(fp,"%s\n","hallo clang");
+	fprintf(fp,"%s\n","hello clang");
 	fprintf(fp,"%s\n","Ç±ÇÒÇ…ÇøÇÌÅAclang");
 	fclose(fp);
 	return 0;
